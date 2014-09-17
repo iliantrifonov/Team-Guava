aff5b50666276f85237a3f95eee40e2aebc6a873
Svetoslav Georgiev <vodkatinibg@yahoo.com>
2014-09-17 17:58:20 +0300

    Added controller.js

---
+++ ExamSystem.FrontEnd/ExamSystem.WebClient/Scripts/Modules/controller.js
@@ -0,0 +1,31 @@
+﻿/// <reference path="data-persister.js" />
+define(["HttpRequester", "DataPersister", "jquery"], function (HttpRequester, DataPersister) {
+    var controller = (function () {
+        var persister = DataPersister.getDataPersister('url');
+        var $main = $('#main');
+
+        function attachEvents() {
+            $main.on('click', '#login-user', login);
+        }
+
+        function Controller() {
+        }
+
+        function login() {
+            var $email = $('email').val();
+            var $password = $('password').val();
+            persister.UserPersister.login($email, $password);
+            window.location.hash = '#/UserLogIn';
+        }
+
+        function renderAllExams() {
+            $main.on('click', '#all-exams')
+        }
+
+        return {
+            renderLogin: renderLogin,
+            attachEvents: attachEvents
+        }
+    })();
+    return controller;
+});
--- ExamSystem.FrontEnd/ExamSystem.WebClient/Scripts/app.js
+++ ExamSystem.FrontEnd/ExamSystem.WebClient/Scripts/app.js
@@ -4,11 +4,14 @@
         paths: {
             jquery: 'Libs/jquery-2.1.1.min',
             Q: 'Libs/q.min',
-            Sammy: 'Libs/sammy-0.7.5'
+            Sammy: 'Libs/sammy-0.7.5',
+            HttpRequester: 'Modules/http-requester',
+            DataRequester: 'Modules/data-persister',
+            controller: 'Modules/controller'
         }
     });
 
-    require(['jquery', 'Sammy'], function (jquery, Sammy) {
+    require(['jquery', 'Sammy', 'controller'], function (jquery, Sammy) {
 
         var app = Sammy('#main', function () {
             this.get("#/", function () {
