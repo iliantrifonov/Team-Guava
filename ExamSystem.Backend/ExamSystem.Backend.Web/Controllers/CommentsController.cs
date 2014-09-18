ecd0414651991b34edfd05a33726139bdbbf9558
iliantrifonov <shogun11@abv.bg>
2014-09-18 08:15:14 +0300

    Small fix to the data model

---
+++ ExamSystem.Backend/ExamSystem.Backend.Web/Controllers/CommentsController.cs
@@ -0,0 +1,79 @@
+﻿namespace ExamSystem.Backend.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+
+    using ExamSystem.Backend.Data;
+    using ExamSystem.Backend.Web.DataModels;
+    using ExamSystem.Backend.Models;
+
+    public class CommentsController : BaseApiController
+    {
+        public CommentsController(IExamSystemData data)
+            : base(data)
+        {
+
+        }
+
+        [HttpGet]
+        public IHttpActionResult All(string examId)
+        {
+            if (string.IsNullOrWhiteSpace(examId) || examId.Length < 36)
+            {
+                return BadRequest();
+            }
+
+            var idAsGuid = new Guid(examId);
+            var exam = this.data.Exams
+                .All()
+                .Where(e => e.Id == idAsGuid)
+                .FirstOrDefault();
+
+            if (exam == null)
+            {
+                return NotFound();
+            }
+
+            var comments = exam.Comments
+                .Select(CommentsDataModel.GetModel());
+
+            return Ok(comments);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Add([FromBody]CommentsDataModel comment)
+        {
+            if (comment == null && !ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            
+            var idAsGuid = new Guid(comment.ExamId);
+            var exam = this.data.Exams
+                .All()
+                .Where(e => e.Id == idAsGuid)
+                .FirstOrDefault();
+
+            if (exam == null)
+            {
+                return NotFound();
+            }
+
+            var commentForDb = new Comment()
+            {
+                Date = DateTime.Now,
+                Text = comment.Text
+            };
+
+            exam.Comments.Add(commentForDb);
+
+            this.data.SaveChanges();
+
+            return Ok(commentForDb.Id);
+        }
+    }
+}
---
+++ ExamSystem.Backend/ExamSystem.Backend.Web/DataModels/CommentsDataModel.cs
@@ -0,0 +1,28 @@
+﻿namespace ExamSystem.Backend.Web.DataModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    using ExamSystem.Backend.Models;
+
+    public class CommentsDataModel
+    {
+        public string Text { get; set; }
+
+        public string ExamId { get; set; }
+
+        public DateTime? Date { get; set; }
+
+        public static Func<Comment, CommentsDataModel> GetModel()
+        {
+            return c => new CommentsDataModel()
+            {
+                Date = c.Date,
+                Text = c.Text,
+                ExamId = c.ExamId == null ? null : c.ExamId.ToString()
+            };
+        }
+    }
+}
